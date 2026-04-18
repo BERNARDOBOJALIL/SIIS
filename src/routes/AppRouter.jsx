16606df70099d32f0347520e4c2be92b3c926116
BERNARDOBOJALIL <bernardo.bojalil.lorenzini@gmail.com>
2026-04-18 10:56:23 -0600

    Visualizador de estado de máquinas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,18 +5,19 @@ import HomePage from '../pages/HomePage'
 import AppointmentsPage from '../pages/AppointmentsPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import SalonesPanel from '../components/panels/SalonesPanel'
+import MaquinasPanel from '../components/panels/MaquinasPanel'
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-  <Route path={ROUTES.HOME} element={<HomePage />} />
-  <Route path={ROUTES.APPOINTMENTS} element={<AppointmentsPage />} />
-  <Route path={ROUTES.SALONES} element={<SalonesPanel />} />
-</Route>
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.APPOINTMENTS} element={<AppointmentsPage />} />
+          <Route path={ROUTES.SALONES} element={<SalonesPanel />} />
+          <Route path={ROUTES.MAQUINAS} element={<MaquinasPanel />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
-        <Route path={ROUTES.SALONES} element={<SalonesPanel />} />
       </Routes>
     </BrowserRouter>
   )
